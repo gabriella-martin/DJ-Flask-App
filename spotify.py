134ab0bdb7d6b62e4d63396f796a2a701e8fccd2
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-03-15 09:43:01 +0000

    code retrival of all track details

--- spotify.py
+++ spotify.py
@@ -13,21 +13,40 @@ class TrackAudioAnalysis:
         self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
         self.track_id = track_id
 
+    def get_track_details(self):
+
+        '''song name, artists on the track & their id, song artwork, popularity, release year '''
+
+        track_details = self.sp.track(self.track_id)
+        song_name = track_details['name']
+        artist_details = []
+        artists = (track_details['artists'])
+        for artist in artists:
+            artist_name = artist['name']
+            artist_id = artist['id']
+            artist_details.append({'artist_name':artist_name, 'artist_id': artist_id})
+        song_image = track_details['album']['images'][2]['url']
+        release_year = (track_details['album']['release_date'])[:4]
+        popularity = track_details['popularity']
+        track_details = {'song_name': song_name, 'artists': artist_details, 'song_image': song_image, 'release_year': release_year, 'popularity':popularity}
+
+        return track_details
+
     def get_audio_features(self):
-        '''
-            danceability - Danceability describes how suitable a track is for dancing based on a combination of musical elements 
+
+        ''' danceability - Danceability describes how suitable a track is for dancing based on a combination of musical elements 
             including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is 
-            most danceable
+            most danceable.
 
             energy - Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, 
             energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on 
             the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, 
-            and general entropy
+            and general entropy.
 
             instrumentalness - Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in 
             this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater 
             likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence 
-            is higher as the value approaches 1.0
+            is higher as the value approaches 1.0.
 
             valence - A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound
             more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
@@ -40,16 +59,22 @@ class TrackAudioAnalysis:
         instrumentalness = audio_features[0]['instrumentalness']
         valence = audio_features[0]['valence']
         bpm = audio_features[0]['tempo']
-        audio_features = [danceability, energy, key, instrumentalness, valence, bpm]
-        for i in audio_features:
-            print(i)
+        audio_features = {'danceability':danceability, 'energy': energy, 'key': key, 'instrumentalness':instrumentalness, 
+                          'valence': valence, 'bpm': bpm}
 
+        return audio_features
 
-'''ya = sp.recommendations(seed_tracks = ['4D2uN3J3sjKL3TUrvY0pS7'])
-print(ya)'''
+    def get_audio_analysis(self):
 
+        '''tempo_confidence - the confidence, from 0.0 to 1.0, of the reliability of the tempo.
+        key_confidence - The confidence, from 0.0 to 1.0, of the reliability of the key.'''
+        audio_analysis = self.sp.audio_analysis(self.track_id)
+        tempo_reliability = (audio_analysis['track']['tempo_confidence'])
+        key_reliability = (audio_analysis['track']['key_confidence'])
+        audio_analysis = {'tempo_reliability': tempo_reliability, 'key_reliability': key_reliability}
+        return audio_analysis
 
 taa = TrackAudioAnalysis('4zN21mbAuaD0WqtmaTZZeP')
-taa.get_audio_features()
+taa.get_track_details()
 
 
