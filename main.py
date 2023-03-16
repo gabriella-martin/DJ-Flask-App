4ec9d06f5e5de68103ebaa42c348919028995ce6
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-03-16 12:36:18 +0000

    add song details

--- main.py
+++ main.py
@@ -17,20 +17,27 @@ def home():
         artist = request.form['artist']
         search_query = song + artist
         taa = TrackAudioAnalysis(search_query)
-        search_query = taa.track_id
-        return redirect(url_for('song_page', song_query=search_query))
+        track_id = taa.track_id
+        return redirect(url_for('song_page', search_query=search_query, track_id=track_id))
     return render_template('index.html', form=form)
 
 
-@app.route('/<song_query>', methods=['GET','POST'] )
-def song_page(song_query):
-    url = f'https://open.spotify.com/embed/track/{song_query}?utm_source=generator&theme=0'
+@app.route('/song/<search_query>/<track_id>', methods=['GET','POST'] )
+def song_page(search_query, track_id):
+    url = f'https://open.spotify.com/embed/track/{track_id}?utm_source=generator&theme=0'
+    taa = TrackAudioAnalysis(search_query)
+    track_details = taa.get_track_details()
+    popularity = track_details['popularity']
+    release_year = track_details['release_year']
+
+    audio_features = taa.get_audio_features()
+    
     if request.method == 'POST':
         if request.form['yesorno'] == 'Yes':
             return redirect(url_for('set_page'))
         else :
             return redirect(url_for('home'))
-    return render_template('song.html', url=url)
+    return render_template('song.html', url=url, popularity=popularity, release_year=release_year, audio_features=audio_features)
 
 @app.route('/set')
 def set_page():
