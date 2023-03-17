88dfd7b04d6958c9e2866bc7a93ed2b659b8a3fd
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-03-17 09:34:55 +0000

    add javascript logic to show/hide personalisation box

---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+document.getElementById('information-box').style.color='#03fcf0'
+
+function show_personalisation() {
+    document.getElementById('personalisation-info-box').style.display='block'
+}
