713f077eec4ca640407ccaed381d240d3c0bd307
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-03-18 13:41:15 +0000

    finish rec coding

--- track_recommendations.py
+++ track_recommendations.py
@@ -2,37 +2,38 @@ import spotipy
 from decouple import config
 from spotipy.oauth2 import SpotifyClientCredentials
 
-#form_results = {'bpm_range': bpm_range, 'key_range':key_range, 'popularity':popularity, 'time_period':time_period, 'danceability':danceability, 'vocal':vocal, 'energy':energy, 'mood':mood}
-#{'song_name': "Don't Stop Just Yet - VIP", 'artists': [{'artist_name': 'Belters Only', 'artist_id': '1H1sDUWSlytzifZTDpKgUA'}, {'artist_name': 'Jazzy', 'artist_id': '7zAAwgV5Wqmvpb4GzvlRkP'}], 'song_image': 'https://i.scdn.co/image/ab67616d00004851ff92c4f7dadc86a44a89e284', 'release_year': '2022', 'popularity': 42, 'danceability': 70, 'energy': 84, 'key': 11, 'vocal': 24, 'mood': 76, 'bpm': 127}
+# python3 track_recommendations.py
+
 class TrackRecommendations():
 
-    def __init__(self,track_id, form_results, track_details):
+    def __init__(self,track_id, form_results, track_features):
         cid = config('SPOTIFY_CLIENT_ID')
         secret = config('SPOTIFY_CLIENT_SECRET')
         client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
         self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
         self.track_id = track_id
         self.form_results = form_results
-        self.track_details = track_details
-
+        self.track_features = track_features
     def get_bpm_ranges(self):
         bpm_range = self.form_results['bpm_range']
-        bpm_of_selected_track = self.track_details['bpm']
-        if bpm_range == 'No preference':
-            max_tempo = None
-            min_tempo = None
+        bpm_of_selected_track = self.track_features['bpm']
+        if bpm_range == 'No Preference':
+            max_tempo = 'NULL'
+            min_tempo = 'NULL'
         else:
-            bpm_range = bpm_range[1:-1]
+            bpm_range = int(bpm_range[1:-1])
             max_tempo = bpm_of_selected_track + bpm_of_selected_track*bpm_range
             min_tempo = bpm_of_selected_track - bpm_of_selected_track*bpm_range
-        return min_tempo, max_tempo
+        tempo = [min_tempo, max_tempo]
+        
+        return tempo
     
     def get_key_ranges(self):
-        key_range = self.form_results['bpm_range']
-        key_of_selected_track = self.track_details['key']
-        if key_range == 'No preference':
-            max_key = None
-            min_key = None
+        key_range = self.form_results['key_range']
+        key_of_selected_track = self.track_features['key']
+        if key_range == 'No Preference':
+            max_key = 'NULL'
+            min_key = 'NULL'
         elif key_range == 'Same Key':
             max_key = key_of_selected_track
             min_key = key_of_selected_track
@@ -46,20 +47,96 @@ class TrackRecommendations():
             else:
                 min_key = key_of_selected_track -1
                 max_key = key_of_selected_track +1
-        return min_key, max_key
+        key = [min_key, max_key]
 
-    def get_time_period(self):
-        max_year = self.form_results['']
+        return key
+
+    def get_popularity_range(self):
+        popularity_of_selected_track = self.track_features['popularity']
+        popularity = self.form_results['popularity']
+        if popularity == 'No Preference':
+            max_popularity = 'NULL'
+            min_popularity = 'NULL'
+        elif popularity == 'More Popular':
+            min_popularity = popularity_of_selected_track
+            max_popularity = 'NULL'
+        else:
+            max_popularity = popularity_of_selected_track
+            min_popularity = 'NULL'
+        popularity = [min_popularity, max_popularity]
+        return popularity
 
 
+    def get_danceability_range(self):
+        danceability_of_selected_track = self.track_features['danceability']
+        danceability = self.form_results['danceability']
+        if danceability == 'No Preference':
+            max_danceability = 'NULL'
+            min_danceability = 'NULL'
+        elif danceability == 'More Danceable':
+            min_danceability = danceability_of_selected_track
+            max_danceability = 'NULL'
+        else:
+            max_danceability = danceability_of_selected_track
+            min_danceability = 'NULL'
+        danceability = [min_danceability, max_danceability]
+        return danceability
+    def get_energy_range(self):
+        energy_of_selected_track = self.track_features['energy']
+        energy = self.form_results['energy']
+        if energy == 'No Preference':
+            max_energy = 'NULL'
+            min_energy = 'NULL'
+        elif energy == 'More Energy':
+            min_energy = energy_of_selected_track
+            max_energy = 'NULL'
+        else:
+            max_energy = energy_of_selected_track
+            min_energy = 'NULL'
+        energy = [min_energy, max_energy]
+        return energy
 
+    def get_mood_range(self):
+        mood_of_selected_track = self.track_features['mood']
+        mood = self.form_results['mood']
+        if mood == 'No Preference':
+            max_mood = 'NULL'
+            min_mood = 'NULL'
+        elif mood == 'Happier':
+            min_mood = mood_of_selected_track
+            max_mood = 'NULL'
+        else:
+            max_mood = mood_of_selected_track
+            min_mood = 'NULL'
 
+        mood = [min_mood, max_mood]
+        return mood
+    
+    def get_recommendations(self):
+        tempo = self.get_bpm_ranges()
+        key = self.get_key_ranges()
+        popularity = self.get_popularity_range()
+        danceability = self.get_danceability_range()
+        energy = self.get_energy_range()
+        mood = self.get_mood_range()
 
-'''
-lets consider options:
+        preferences_list = tempo + key + popularity + danceability + energy + mood
+        for index, preference in enumerate(preferences_list):
+            if preference == 'NULL':
+                preferences_list[index] = None
 
-    BPM RANGE OPTIONS: +-3% +-5% +-8%
-    KEY COMPLIMENTS: same key, complimentary key, dont care
-    POPULARITY, DANCE, VOCAL, MOOD, ENERGY: more, less, don't care 
+        recommendations = self.sp.recommendations(seed_tracks =[self.track_id], min_tempo=preferences_list[0], max_tempo=preferences_list[1], min_key=preferences_list[2],
+                                                  max_key=preferences_list[3], max_popularity=preferences_list[4], min_popularity=preferences_list[5],
+                                                   min_danceability=preferences_list[6],
+                                                  max_danceability=preferences_list[7], min_energy=preferences_list[8], max_energy=preferences_list[9], min_valence = preferences_list[10],
+                                                  max_valence=preferences_list[11], limit=10)
+        
+        urls = []
+        tracks = recommendations['tracks']
+        for track in tracks:
+            track_id = track['id']
+            track_embed_link = f'https://open.spotify.com/embed/track/{track_id}?utm_source=generator&theme=0'
+            urls.append(track_embed_link)
 
-'''
+        return urls
+
