145d78f6b5caa1b31ea3b763f8c5d5468e00ee68
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-03-18 13:43:44 +0000

    push css and javascript

--- static/js/index.js
+++ static/js/index.js
@@ -2,4 +2,12 @@ document.getElementById('information-box').style.color='#03fcf0'
 
 function show_personalisation() {
     document.getElementById('personalisation-info-box').style.display='block'
+    document.getElementById('right-song-question').style.display='none'
+    document.getElementById('line-break').style.display='none'
+    
+}
+
+function back_to_search(){
+
+    window.location.href = "/";
 }
